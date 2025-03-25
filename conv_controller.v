3b17ac2b3a68b8863beef0499826550a6bd15744
MahantaTalakal <148555051+MahantaTalakal@users.noreply.github.com>
2025-03-25 20:55:39 +0530

    Update conv_controller.v

--- conv_controller.v
+++ conv_controller.v
@@ -3,27 +3,27 @@
 module conv_controller #(parameter dim=28)(
     input clk, rst, start,
     input [7:0] pixel_in,
-    output wire [19:0] conv_out,
+    output wire [15:0] conv_out,
     output reg done,
-    output wire load_done,conv_done
+    output wire load_done,conv_done,pool_done
     );
 
     reg [8:0] i, j;
     reg [9:0] pixel_count;
-    reg [7:0] input_image[0:783];  
-    reg [20:0] conv_result[0:675];
+    reg [15:0] input_image[0:783];  
+    reg [15:0] conv_result[0:675];
     
-    wire [7:0] input_kernel[0:8];
+    wire [15:0] input_kernel[0:8];
     
-    assign input_kernel[0] = 8'b00010011; 
-    assign input_kernel[1] = 8'b11011000; 
-    assign input_kernel[2] = 8'b01100010; 
-    assign input_kernel[3] = 8'b00110010; 
-    assign input_kernel[4] = 8'b01011110; 
-    assign input_kernel[5] = 8'b01110110; 
-    assign input_kernel[6] = 8'b00111011; 
-    assign input_kernel[7] = 8'b01000110; 
-    assign input_kernel[8] = 8'b11110100; 
+    assign input_kernel[0] = {8'b0,8'b00010011}; 
+    assign input_kernel[1] = {8'b0,8'b11011000}; 
+    assign input_kernel[2] = {8'b0,8'b01100010}; 
+    assign input_kernel[3] = {8'b0,8'b00110010}; 
+    assign input_kernel[4] = {8'b0,8'b01011110}; 
+    assign input_kernel[5] = {8'b0,8'b01110110}; 
+    assign input_kernel[6] = {8'b0,8'b00111011}; 
+    assign input_kernel[7] = {8'b0,8'b01000110}; 
+    assign input_kernel[8] = {8'b0,8'b11110100}; 
 
     localparam [3:0] IDLE = 4'd0,
                      LOAD_IMAGE = 4'd1,
@@ -34,30 +34,30 @@ module conv_controller #(parameter dim=28)(
                      DONE = 4'd6;
 
     reg [2:0] state_reg, state_next;
-    wire [7:0] pixel_in_window[0:8];
-    reg [19:0] conv_out_reg [675:0];
-    reg [19:0] max_pool_out [0:168];
+    wire [15:0] pixel_in_window[0:8];
+    reg [15:0] conv_out_reg [675:0];
+    reg [15:0] max_pool_out [0:168];
     reg [3:0] pool_i, pool_j;
-    wire [19:0] a, b, c, d;
-    wire [19:0] conv_out_temp;
-    wire[79:0] pool_window;
+    wire [15:0] a, b, c, d;
+    wire [15:0] conv_out_temp;
+    wire[63:0] pool_window;
 
-    assign pixel_in_window[0] = input_image[(dim * i) + j];     
-    assign pixel_in_window[1] = input_image[(dim * i) + (j+1)];
-    assign pixel_in_window[2] = input_image[(dim * i) + (j+2)];
-    assign pixel_in_window[3] = input_image[(dim * (i+1)) + j];     
-    assign pixel_in_window[4] = input_image[(dim * (i+1)) + (j+1)];
-    assign pixel_in_window[5] = input_image[(dim * (i+1)) + (j+2)];
-    assign pixel_in_window[6] = input_image[(dim * (i+2)) + j];     
-    assign pixel_in_window[7] = input_image[(dim * (i+2)) + (j+1)];
-    assign pixel_in_window[8] = input_image[(dim * (i+2)) + (j+2)];
+    assign pixel_in_window[0] = {input_image[(dim * i) + j],8'b0};     
+    assign pixel_in_window[1] = {input_image[(dim * i) + (j+1)],8'b0};
+    assign pixel_in_window[2] = {input_image[(dim * i) + (j+2)],8'b0};
+    assign pixel_in_window[3] = {input_image[(dim * (i+1)) + j],8'b0};     
+    assign pixel_in_window[4] = {input_image[(dim * (i+1)) + (j+1)],8'b0};
+    assign pixel_in_window[5] = {input_image[(dim * (i+1)) + (j+2)],8'b0};
+    assign pixel_in_window[6] = {input_image[(dim * (i+2)) + j],8'b0};     
+    assign pixel_in_window[7] = {input_image[(dim * (i+2)) + (j+1)],8'b0};
+    assign pixel_in_window[8] = {input_image[(dim * (i+2)) + (j+2)],8'b0};
     
     assign a = conv_out_reg[(2 * pool_i) * 26 + (2 * pool_j)];
     assign b = conv_out_reg[(2 * pool_i) * 26 + (2 * pool_j + 1)];
     assign c = conv_out_reg[(2 * pool_i + 1) * 26 + (2 * pool_j)];
     assign d = conv_out_reg[(2 * pool_i + 1) * 26 + (2 * pool_j + 1)];
 
-    wire [71:0] pixel_window, kernel_window;
+    wire [143:0] pixel_window, kernel_window;
     
     assign pixel_window = {pixel_in_window[8], pixel_in_window[7], pixel_in_window[6],
                            pixel_in_window[5], pixel_in_window[4], pixel_in_window[3],
@@ -171,6 +171,7 @@ module conv_controller #(parameter dim=28)(
     
     assign load_done = pixel_count >= 784 ? 1 : 0 ;
     assign conv_done = (i >= 26) ? 1 : 0;
+    assign pool_done = (pool_i >= 13) ? 1 : 0;
 
     
 endmodule
